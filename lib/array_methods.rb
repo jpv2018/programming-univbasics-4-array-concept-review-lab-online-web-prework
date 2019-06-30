0920d8a610457b1f9e1792ea102371d343095b28
Jason Viola <jason.viola@mavs.uta.edu>
2019-06-30 14:51:56 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ array.index do |x| x == value_to_find end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
